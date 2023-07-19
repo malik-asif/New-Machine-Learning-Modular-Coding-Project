548a04ff32de370e6cf3be44aae131c9cb08de80
malikasif <er.malikasif@gmail.com>
2023-07-19 18:28:44 +0530

    'update'

---
+++ template.py
@@ -0,0 +1,48 @@
+import os
+import sys
+from pathlib import Path
+import logging
+
+while True:
+    project_name = input('Enter your project name: ')
+    if project_name != '':
+        break
+
+# src/__init__.py
+# src/components/__init__.py
+list_of_files = [
+    f"{project_name}/__init__.py",
+    f"{project_name}/components/__init__.py",
+    f"{project_name}/config/__init__.py",
+    f"{project_name}/constants/__init__.py",
+    f"{project_name}/entity/__init__.py",
+    f"{project_name}/exception/__init__.py",
+    f"{project_name}/logger/__init__.py",
+    f"{project_name}/pipeline/__init__.py",
+    f"{project_name}/utils/__init__.py",
+    f"config/config.yaml",
+    "schema.yaml",
+    "app.py",
+    "main.py",
+    "logs.py",
+    "exception.py",
+    "setup.py"
+]
+
+
+for filepth in list_of_files:
+    filepath = Path(filepth)
+    # define file directory and filename & split all required folder in src subfolders
+    filedir, filename = os.path.split(filepath)
+
+    if filedir != "":
+        # check if filedir empty than create folders, exit_ok = true means if available than not create folders.
+        os.makedirs(filedir, exist_ok=True)
+
+    # 1st condition check file path exist or not & 2nd condition list_of_file not avaolable than generate
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath, 'w') as f:
+            pass
+
+    else:
+        logging.info('file is already present at : {filepath}')
